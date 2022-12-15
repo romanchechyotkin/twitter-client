97c7e3addc43668a1f54af4ac647caba93fefb80
Roman Chechyotkin <romanchechyotkin@gmail.com>
2022-12-15 10:11:08 +0300

    fixed page navbar

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {PageNavbar, PageNavbarIconEnum} from "shared/ui/PageNavbar/PageNavbar";
 import {CreateTweetForm} from "features/CreateTweet";
 import {Feed} from "widgets/Feed";
+import cls from './HomePage.module.scss'
 
 const HomePage = () => {
 
     return (
-        <div>
+        <div className={cls.homePage}>
             <PageNavbar navbarTitle={'Home'} icon={PageNavbarIconEnum.HOME}/>
             <CreateTweetForm />
             <Feed />
